fb9da80491f059c70a94fda1357404602b5aec29
EmilioPlus <jaiber18w@hotmail.com>
2021-10-11 02:42:23 -0500

    Actualizacion del API con CROS

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -13,21 +13,20 @@ namespace API.Controllers
         private readonly DataContext _context;
         public ActivitiesController(DataContext context)
         {
-            _context = context;
+        _context = context;
         }
         
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+        return await _context.Activities.ToListAsync();
         }
         
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+        return await _context.Activities.FindAsync(id);
         }
-
-      
     }
 }
+
