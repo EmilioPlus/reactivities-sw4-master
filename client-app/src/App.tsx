ec911ebbab378230a6d97ea9f08264003c3b3561
EmilioPlus <67715492+EmilioPlus@users.noreply.github.com>
2021-11-18 12:38:19 -0500

    Update App.tsx

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,31 +2,29 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
-  const [activities, setActivities] = useState([]);
 
-  useEffect(()=>{
-    axios.get('https://localhost:5001/Activities').then(response => {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then( response =>{
       console.log(response);
       setActivities(response.data);
     })
   }, [])
 
   return (
-    <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {activities.map((activity:any) => (
-            <li key={activity.id}>
-              {activity.title} -  {activity.date}
-            </li>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+      <List>
+      {activities.map((activity: any) =>(
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
           ))}
-        </ul>
-
-    </header>
-  </div>
+      </List>
+    </div>
   );
 }
 
